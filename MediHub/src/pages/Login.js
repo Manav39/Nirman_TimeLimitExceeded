28e4158da9cbaf50b49e820827dd6e4600050d01
TIDYMOUSE <eulerandfriends@gmail.com>
2024-05-03 08:46:03 +0530

    final changes

--- MediHub/src/pages/Login.js
+++ MediHub/src/pages/Login.js
@@ -32,7 +32,6 @@ const Login = () => {
   const toast = useToast();
   const { authData } = useContract();
 
-  const { account, setUserType } = useContract();
   const { setAccount, setUserType, account } = useContract();
 
   const [name, setName] = useState("");
