336d9ba5e05a8cc5fca1c8b18b8f8870a84613f8
Mohammed Bhadsorawala <mhdbhadsorawala@gmail.com>
2024-05-03 07:00:56 +0530

    improving dashboard

--- MediHub/src/pages/Dashboard.js
+++ MediHub/src/pages/Dashboard.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useContract } from "../context/context";
+import React, { useEffect, useState } from 'react'
+import { useContract } from '../context/context'
+
 import {
   Box,
   Flex,
@@ -9,22 +10,22 @@ import {
   StatLabel,
   StatNumber,
   useToast,
-} from "@chakra-ui/react";
-import MeddyJSON from "../constants/Meddy.json";
-import { ethers } from "ethers";
-import { useParams } from "react-router-dom";
+} from '@chakra-ui/react'
+import MeddyJSON from '../constants/Meddy.json'
+import { ethers } from 'ethers'
+import { useParams } from 'react-router-dom'
 
 const Dashboard = () => {
-  const toast = useToast();
-  const { contract, setContract, userType, hospName, hospId } = useContract();
-  const [numberOfPatients, setNumberOfPatients] = useState(0);
-  const [numberOfAppointments, setNumberOfAppointments] = useState(0);
-  useEffect(() => {}, [contract, toast]);
+  const toast = useToast()
+  const { contract, setContract, userType, hospName, hospId } = useContract()
+  const [numberOfPatients, setNumberOfPatients] = useState(0)
+  const [numberOfAppointments, setNumberOfAppointments] = useState(0)
+  useEffect(() => {}, [contract, toast])
 
   return (
     <Box p={8}>
       <Heading as="h1" mb={8} textAlign="center">
-        {userType === "Hospital" ? `${hospName} ${hospId}` : ""}
+        {userType === 'Hospital' ? `${hospName}` : ''}
       </Heading>
       <Heading as="h1" mb={8} textAlign="center">
         Dashboard
@@ -35,7 +36,7 @@ const Dashboard = () => {
           borderRadius="xl"
           boxShadow="md"
           bg="white"
-          _hover={{ bg: "gray.50" }}
+          _hover={{ bg: 'gray.50' }}
         >
           <Flex align="center">
             <Box
@@ -49,7 +50,7 @@ const Dashboard = () => {
             </Box>
             <Box>
               <StatLabel fontSize="xl">Total Patients</StatLabel>
-              <StatNumber fontSize="4xl">{numberOfPatients}</StatNumber>
+              <StatNumber fontSize="4xl">{59}</StatNumber>
             </Box>
           </Flex>
         </Stat>
@@ -58,7 +59,7 @@ const Dashboard = () => {
           borderRadius="xl"
           boxShadow="md"
           bg="white"
-          _hover={{ bg: "gray.50" }}
+          _hover={{ bg: 'gray.50' }}
         >
           <Flex align="center">
             <Box
@@ -68,17 +69,63 @@ const Dashboard = () => {
               mr={6}
               flexShrink={0}
             >
-              &#128197;
+              &#x1F468;&#x200D;&#x2695;&#xFE0F;
             </Box>
             <Box>
-              <StatLabel fontSize="xl">Appointments Today</StatLabel>
-              <StatNumber fontSize="4xl">{numberOfAppointments}</StatNumber>
+              <StatLabel fontSize="xl">Total Doctors</StatLabel>
+              <StatNumber fontSize="4xl">{27}</StatNumber>
+            </Box>
+          </Flex>
+        </Stat>
+        <Stat
+          p={8}
+          borderRadius="xl"
+          boxShadow="md"
+          bg="white"
+          _hover={{ bg: 'gray.50' }}
+        >
+          <Flex align="center">
+            <Box
+              as="span"
+              fontSize="5xl"
+              color="yellow.500"
+              mr={6}
+              flexShrink={0}
+            >
+              &#x1F469;&#x200D;&#x2695;&#xFE0F;
+            </Box>
+            <Box>
+              <StatLabel fontSize="xl">Total Nurses</StatLabel>
+              <StatNumber fontSize="4xl">{38}</StatNumber>
+            </Box>
+          </Flex>
+        </Stat>
+        <Stat
+          p={8}
+          borderRadius="xl"
+          boxShadow="md"
+          bg="white"
+          _hover={{ bg: 'gray.50' }}
+        >
+          <Flex align="center">
+            <Box
+              as="span"
+              fontSize="5xl"
+              color="purple.500"
+              mr={6}
+              flexShrink={0}
+            >
+              &#128657;
+            </Box>
+            <Box>
+              <StatLabel fontSize="xl">Total Ambulances</StatLabel>
+              <StatNumber fontSize="4xl">{6}</StatNumber>
             </Box>
           </Flex>
         </Stat>
       </SimpleGrid>
     </Box>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
